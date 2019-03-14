59c2f3075f5b8c0ecd84c77e699152956813bb1a
Roli B <rolib2@gmail.com>
2019-03-13 20:03:13 -0400

    Small fix

--- src/container/DoctorListContainer.js
+++ src/container/DoctorListContainer.js
@@ -1,9 +1,11 @@
 import {connect} from 'react-redux'
 import DoctorList from '../components/DoctorList'
-import { getDoctorsApi, deleteDoctor } from '../services/DoctorService'
-import {CargarDoctores, DeleteDoctor} from '../actions/actions'
+import {CargarDoctores, DeleteDoctor, GetDoctorsAction, DeleteDoctorAction} from '../actions/actions'
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownprops) => {
+    const match  = ownprops.match
+    const id = match.params.id
+
     return {
         doctors: state.doctors,
         direccion: 'en cochabamba'
@@ -13,20 +15,10 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         getDoctors: () => {
-            getDoctorsApi().then(res => {
-                dispatch(CargarDoctores(res.data))
-            }).catch(err => {
-                console.log(err)
-            })
+            dispatch(GetDoctorsAction())
         },
         deleteDoctor: (doctor_id) => {
-            deleteDoctor(doctor_id).then(res => {
-                dispatch(DeleteDoctor(doctor_id))
-            }).catch(err => {
-                console.log(err)
-            })
-            //TODO eliminar doctor en el api
-            // console.log(' todo implement delete doctor', doctor_id)
+            dispatch(DeleteDoctorAction(doctor_id))
         }
     }
 }
