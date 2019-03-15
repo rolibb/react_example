5fc7d53ad117056996c2975903ce351a8f2c4c96
Roli B <rolib2@gmail.com>
2019-03-14 20:47:08 -0400

    Add doctor view

--- src/components/DoctorProfile.js
+++ src/components/DoctorProfile.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const DoctorProfile = ({}) => (
+const DoctorProfile = ({doctor}) => (
     <div className="row">
-        <div className="col-12">compoenet DoctorProfile</div>
+        <div className="col-4">
+            <div className="row">
+                <h1 className="col-12">
+                Perfil del doctor
+                </h1>
+                <div className="col-12">Nombres: {doctor.nombres}</div>
+                <div className="col-12">Apellidos: {doctor.apellidos}</div>
+                <div className="col-12">Telefono: {doctor.telefono}</div>
+            </div>
+        </div>
+        <div className="col-8">Formulario para agregar ubicaciones de consultorio</div>
     </div>
 )
 
--- src/container/DoctorProfileContainer.js
+++ src/container/DoctorProfileContainer.js
@@ -1,8 +1,26 @@
 import {connect} from 'react-redux'
 import DoctorProfile from '../components/DoctorProfile'
 
+const findDoctor = (doctors, id) => {
+    console.log(id)
+    console.log(doctors)
+
+    const doctor = doctors.filter(doctor => doctor.id == id)
+
+    if (doctor.length > 0) {
+        console.log(doctor[0])
+        return doctor[0]
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        doctor: findDoctor(state.doctors.doctors, ownProps.match.params.doctor_id)
+    }
+}
+
 const DoctorProfileContainer = connect(
- null,
+ mapStateToProps,
  null
 )(DoctorProfile)
 
